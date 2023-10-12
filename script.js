297e8393730046fad773d36caac742d49b6e0f3d
Joao Vitor cardoso <sr.melovitor@gmail.com>
2023-10-11 22:29:56 -0300

    Novos estilos , Sobre mim e habilidades

--- script.js
+++ script.js
@@ -1,12 +1,18 @@
-function digitar(elemento){
+
+   function digitar(elemento){
     const texto = elemento.innerHTML.split('');
     elemento.innerHTML ='';
     texto.forEach((letra,i) => {
          setTimeout(()=>{
             elemento.innerHTML += letra;
          } ,75 * i);
-    })
+    });
 }
 const titulo = document.querySelector('.digitando');
 
 digitar(titulo);
+
+const menu = document.querySelector('.fa-bars');
+menu.addEventListener('click',()=>{
+    menu.classList.toggle('fa-x')
+})
