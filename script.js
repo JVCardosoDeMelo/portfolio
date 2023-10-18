8d9866c52cf9b99262b25ec3222fdf6ab0221394
Joao Vitor cardoso <sr.melovitor@gmail.com>
2023-10-17 23:04:16 -0300

    Habilidades e curiosidades

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-
+function escreverletra(){
    function digitar(elemento){
     const texto = elemento.innerHTML.split('');
     elemento.innerHTML ='';
@@ -11,11 +11,64 @@
 const titulo = document.querySelector('.digitando');
 
 digitar(titulo);
+}
 
-const menu = document.querySelector('.fa-bars');
+escreverletra();
+
+
+function menuMobol(){const menu = document.querySelector('.fa-bars');
 const nav = document.querySelector('header .nav-1');
 
 menu.addEventListener('click',()=>{
     menu.classList.toggle('fa-x')
     nav.classList.toggle('ativado')
-})
+});
+}
+
+function sobremim(){
+
+  const divexperiencia = document.querySelectorAll('.experiencia-content div');
+  const liexperiencia = document.querySelectorAll('.experiencia-content ul li');
+  const diveducacao = document.querySelectorAll('.educacao-content div');
+  const lieducacao = document.querySelectorAll('.educacao-content ul li');
+
+  divexperiencia[0].classList.add('ativo');
+  liexperiencia[0].classList.add('ativo');
+  diveducacao[0].classList.add('ativo');
+  lieducacao[0].classList.add('ativo');
+
+
+  function slideshow(index){
+    divexperiencia.forEach((div)=>{
+        div.classList.remove('ativo');
+    });
+    liexperiencia.forEach((botao)  =>{
+       botao.classList.remove('ativo')
+    })
+    divexperiencia[index].classList.add('ativo')
+    liexperiencia[index].classList.add('ativo')
+}
+  function slideshow2(index){
+    diveducacao.forEach((div)=>{
+        div.classList.remove('ativo');
+    });
+    lieducacao.forEach((botao)  =>{
+       botao.classList.remove('ativo')
+    });
+    diveducacao[index].classList.add('ativo')
+    lieducacao[index].classList.add('ativo')
+}
+
+liexperiencia.forEach((evento,index) =>{
+    evento.addEventListener('click', () =>{
+        slideshow(index)
+    });
+});
+lieducacao.forEach((evento,index) =>{
+    evento.addEventListener('click', () =>{
+        slideshow2(index)
+    });
+});
+}
+
+sobremim();
